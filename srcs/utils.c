0594b2b098d345c1ee5b3be4fd7b11c1b619a84e
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2025-10-03 14:48:31 +0200

    Add global t_game struct

--- srcs/utils.c
+++ srcs/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 13:16:29 by omizin            #+#    #+#             */
-/*   Updated: 2025/10/01 13:27:35 by omizin           ###   ########.fr       */
+/*   Updated: 2025/10/03 14:48:02 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,11 @@ void	print_error(char *msg)
 	write(2, RED, 6);
 	write(2, msg, ft_strlen(msg));
 	write(2, "\n"RESET, 6);
+}
+
+t_game	*ft_game(void)
+{
+	static t_game	game;
+
+	return (&game);
 }
