bc19ef505b473d3c90c78d9dfefeea4e3b7d9408
SuPuHe <mizin.a.s.2006@gmail.com>
2025-11-06 13:49:45 +0100

    make doors.c due norm

--- srcs/doors.c
+++ srcs/doors.c
@@ -6,17 +6,29 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 15:17:38 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/11/05 12:36:27 by omizin           ###   ########.fr       */
+/*   Updated: 2025/11/06 13:44:25 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
+static void	init_door_positions(t_game *game, int x, int y)
+{
+	t_door	*door;
+
+	door = &game->doors[game->door_count];
+	door->x = x;
+	door->y = y;
+	door->progress = 0.0;
+	door->state = DOOR_CLOSED;
+	door->timer = 0;
+	game->door_count++;
+}
+
 void	init_doors(t_game *game)
 {
 	int		y;
 	int		x;
-	t_door	*door;
 
 	game->door_count = 0;
 	y = 0;
@@ -29,13 +41,7 @@ void	init_doors(t_game *game)
 			{
 				if (game->door_count >= 64)
 					break ;
-				door = &game->doors[game->door_count];
-				door->x = x;
-				door->y = y;
-				door->progress = 0.0;
-				door->state = DOOR_CLOSED;
-				door->timer = 0;
-				game->door_count++;
+				init_door_positions(game, x, y);
 			}
 			x++;
 		}
@@ -66,12 +72,52 @@ static bool	is_player_in_doorway(t_game *game, t_door *door)
 
 	player_map_x = (int)game->player.x;
 	player_map_y = (int)game->player.y;
-
 	// Check if player is in the same cell as door
 	if (player_map_x == door->x && player_map_y == door->y)
 		return (true);
-
 	return (false);
+}
+
+static void	door_state_opening(t_door *door)
+{
+	door->progress += DOOR_OPEN_SPEED;
+	if (door->progress >= 1.0)
+	{
+		door->progress = 1.0;
+		door->state = DOOR_OPEN;
+		door->timer = DOOR_OPEN_TIME;
+	}
+}
+
+static void	door_state_open(t_door *door, t_game *game)
+{
+	door->timer--;
+	if (door->timer <= 0)
+	{
+		// Start closing if player not in doorway
+		if (!is_player_in_doorway(game, door))
+			door->state = DOOR_CLOSING;
+		else
+			door->timer = 10; // retry in a bit
+	}
+}
+
+static void	door_state_closing(t_door *door, t_game *game)
+{
+	// Check if player entered doorway while closing
+	if (is_player_in_doorway(game, door))
+	{
+		door->state = DOOR_OPENING;
+	}
+	else
+	{
+		door->progress -= DOOR_CLOSE_SPEED;
+		if (door->progress <= 0.0)
+		{
+			door->progress = 0.0;
+			door->state = DOOR_CLOSED;
+		}
+	}
 }
 
 // Update all doors (call every frame)
@@ -86,82 +132,33 @@ void	update_doors(void *param)
 	while (i < game->door_count)
 	{
 		door = &game->doors[i];
-
 		if (door->state == DOOR_OPENING)
-		{
-			door->progress += DOOR_OPEN_SPEED;
-			if (door->progress >= 1.0)
-			{
-				door->progress = 1.0;
-				door->state = DOOR_OPEN;
-				door->timer = DOOR_OPEN_TIME;
-			}
-		}
+			door_state_opening(door);
 		else if (door->state == DOOR_OPEN)
-		{
-			door->timer--;
-			if (door->timer <= 0)
-			{
-				// Start closing if player not in doorway
-				if (!is_player_in_doorway(game, door))
-					door->state = DOOR_CLOSING;
-				else
-					door->timer = 10; // retry in a bit
-			}
-		}
+			door_state_open(door, game);
 		else if (door->state == DOOR_CLOSING)
-		{
-			// Check if player entered doorway while closing
-			if (is_player_in_doorway(game, door))
-			{
-				door->state = DOOR_OPENING;
-			}
-			else
-			{
-				door->progress -= DOOR_CLOSE_SPEED;
-				if (door->progress <= 0.0)
-				{
-					door->progress = 0.0;
-					door->state = DOOR_CLOSED;
-				}
-			}
-		}
+			door_state_closing(door, game);
 		i++;
 	}
 }
 
-// Interact with door (E key or use button)
-void	interact_with_door(t_game *game)
+static double	get_distance(t_game *game, t_door *door)
 {
-	int		map_x;
-	int		map_y;
-	t_door	*door;
 	double	dx;
 	double	dy;
 	double	dist;
 
-	// Check tile in front of player
-	map_x = (int)(game->player.x + game->player.dir_x * DOOR_INTERACTION_DIST);
-	map_y = (int)(game->player.y + game->player.dir_y * DOOR_INTERACTION_DIST);
-
-	door = find_door_at(game, map_x, map_y);
-	if (!door)
-		return ;
-
-	// Calculate distance to door center
 	dx = (door->x + 0.5) - game->player.x;
 	dy = (door->y + 0.5) - game->player.y;
 	dist = sqrt(dx * dx + dy * dy);
+	return (dist);
+}
 
-	// Only interact if close enough
-	if (dist > DOOR_INTERACTION_DIST)
-		return ;
-
+static void	change_door_state(t_game *game, t_door *door)
+{
 	// Toggle door state
 	if (door->state == DOOR_CLOSED)
-	{
 		door->state = DOOR_OPENING;
-	}
 	else if (door->state == DOOR_OPEN)
 	{
 		// Manual close
@@ -178,4 +175,26 @@ void	interact_with_door(t_game *game)
 		// Reverse to opening
 		door->state = DOOR_OPENING;
 	}
+}
+
+// Interact with door (E key or use button)
+void	interact_with_door(t_game *game)
+{
+	int		map_x;
+	int		map_y;
+	t_door	*door;
+	double	dist;
+
+	// Check tile in front of player
+	map_x = (int)(game->player.x + game->player.dir_x * DOOR_INTERACTION_DIST);
+	map_y = (int)(game->player.y + game->player.dir_y * DOOR_INTERACTION_DIST);
+	door = find_door_at(game, map_x, map_y);
+	if (!door)
+		return ;
+	// Calculate distance to door center
+	dist = get_distance(game, door);
+	// Only interact if close enough
+	if (dist > DOOR_INTERACTION_DIST)
+		return ;
+	change_door_state(game, door);
 }
