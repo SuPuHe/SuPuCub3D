65d696935ad85e91c565c9f7e06b4dd63d1a9ec5
SuPuHe <mizin.a.s.2006@gmail.com>
2025-11-10 12:24:19 +0100

    add comments for texterues ui and utils

--- srcs/textures.c
+++ srcs/textures.c
@@ -6,12 +6,24 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 14:03:36 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/11/10 11:15:12 by omizin           ###   ########.fr       */
+/*   Updated: 2025/11/10 12:22:47 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
+/**
+ * @brief Loads additional wall textures from predefined file paths.
+ *
+ * Allocates memory for all wall textures and loads each one using
+ * the MiniLibX `mlx_load_png()` function. If any texture fails to load,
+ * an error message is displayed and the game exits.
+ *
+ * @param game Pointer to the main game structure containing texture data.
+ *
+ * @note This function assumes that `game->textures.wall_tex_paths` and
+ *       `game->textures.wall_tex_count` have already been initialized.
+ */
 static void	more_walls_textures_load(t_game *game)
 {
 	int	i;
@@ -32,6 +44,19 @@ static void	more_walls_textures_load(t_game *game)
 	}
 }
 
+/**
+ * @brief Initializes file paths for extra wall textures.
+ *
+ * Allocates memory for wall texture paths and fills them with
+ * hardcoded texture file locations. The number of wall textures
+ * is set to 8, and the corresponding file paths are stored in
+ * `game->textures.wall_tex_paths`.
+ *
+ * @param game Pointer to the main game structure.
+ *
+ * @note The function uses `ft_strdup()` to duplicate the path strings,
+ *       which should be freed later to avoid memory leaks.
+ */
 static void	more_walls_paths(t_game *game)
 {
 	game->textures.wall_tex_count = 8;
@@ -47,6 +72,16 @@ static void	more_walls_paths(t_game *game)
 	game->textures.wall_textures = NULL;
 }
 
+/**
+ * @brief Loads all required game textures including walls, doors, and GUI.
+ *
+ * Loads the main directional wall textures (north, south, east, west),
+ * as well as the door and GUI textures. Then calls helper functions
+ * to set up and load additional wall textures from predefined paths.
+ *
+ * @note If any texture fails to load, an error is printed and the
+ *       program exits via `exit_game(1)`.
+ */
 void	textures_load(void)
 {
 	t_game	*game;
